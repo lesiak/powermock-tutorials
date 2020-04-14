e1babc0cad3fa56eccf1f7b9b0ab126de1a4a25f
lesiak <lesiak@outlook.com>
2020-04-14 08:19:46 +0200

    Added powerMockSpring project

---
+++ powerMockSpring/src/main/java/com/example/powerMockSpring/PowerMockSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.powerMockSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerMockSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerMockSpringApplication.class, args);
+	}
+
+}
---
+++ powerMockSpring/src/test/java/com/example/powerMockSpring/PowerMockSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.powerMockSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowerMockSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
